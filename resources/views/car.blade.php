4820c49db213f6ac92efc9eeaa255c0c19a06efc
Malc <mill-4456@hotmail.com>
2024-03-10 21:25:13 +0700

    Update

--- resources/views/car.blade.php
+++ resources/views/car.blade.php
@@ -3,7 +3,11 @@
 
 @section('content')
 <h2>ข้อมูลรถ</h2>
-
         <p>ใส่ตาราง</p>
-    
+        <table class="table">
+                <tr><td>ทะเบียนรถ</td><td>รุ่นรถ</td><td>แบรนด์</td><td>เลขคัสซี</td><td>สีรถ</td><td>ชิ่อ</td><td>สกุล</td><td>เบอร์</td><td>ลบ</td><td>แก้ไข</td></tr>
+                <?php 
+                        echo "<tr><td>#</td><td>#</td><td>#</td><td>#</td><td>#</td><td>#</td><td>#</td><td>#</td><td><button>ลบ</button></td><td><button>แก้ไข</button></td></tr>";
+                
+                 echo "</table>";?> 
 @endsection
