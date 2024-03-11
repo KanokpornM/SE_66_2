9ec346996ce39e1d5e6c8f2abfaa4c710bf212aa
Surachai2545 <Cokezacup01@gmail.com>
2024-03-11 22:22:00 +0700

    อันนี้เพิ่มส่วน partlist quotation receipt
    พวกใบสั่งซ่อม ใบเสร็จ ใบเสนอราคา

--- resources/views/partlist.blade.php
+++ resources/views/partlist.blade.php
@@ -4,6 +4,68 @@
 @section('content')
 <h2>ใบสั่งซ่อม</h2>
 
-        <p>ใส่ตาราง</p>
+<style>
+        .card {
+            margin: 20px;
+            border: 4px solid #040202;
+            border-radius: 8px;
+            padding: 20px;
+        }
+        .table {
+                border: 2px solid #040202;
+                
+        }
+        .card-header {
+        text-align: center; 
+        }
+    </style>
+
+
+    <div class="container">
+        <div class="card">
+            <div class="card-header " >
+                <h3>สั่งซ่อม</h3>
+            </div>
+
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>วันที่สั่งซ่อม</p>
+                        <p>ทะเบียนรถ</p>
+                        <p>ชื่อลูกค้า</p>
+                    </div>
+                    <div class="col-md-6">
+                        <p>เลขคัสซี</p>
+                        <p>รุ่นรถ</p>
+                    </div>
+                </div>
+            </div>
+
+                
+                <table class="table table-bordered">
+                        <tr>
+                            <td style="text-align:center">อาการ</td>
+                            <td style="text-align:center">รหัสสินค้า</td>
+                            <td style="text-align:center">อะไหล่</td>
+                            <td style="text-align:center">จำนวน</td>
+                            
+                   
+                        </tr>
+
+                        @php
+            for ($x = 0; $x < 5; $x++) {
+                echo "<tr>
+                        <td style='text-align:center'>ล้อรงอ</td>
+                        <td style='text-align:center'>ล้อ</td>
+                        <td style='text-align:center'>4000</td>
+                        <td style='text-align:center'>2</td>
+                               
+                    </tr>";
+            }
+        @endphp
+                </table>
+            </div>
+        </div>
+    </div>
     
 @endsection
