e2be7176fc7efd6b489d894bfada4ac3de1d8a89
mon2384 <152243939+bart2559b@users.noreply.github.com>
2024-03-13 22:43:00 +0700

    rename file

--- resources/views/carcheck.blade.php
+++ resources/views/carcheck.blade.php
@@ -1,35 +1,26 @@
 @extends('layout')
-@section('title', 'Carcheck')
+@section('title', 'Carrecive')
 
 @section('content')
-    <h2>ข้อมูลรับรถ</h2>
+    <h2>ประวัติอาการรถ</h2>
+
     <p class="">ID รับรถ</p>
     <table class="table table-bordered text-center">
         <tr>
-            <td>ชิ่อ</td>
-            <td>สกุล</td>
-            <td>เบอร์</td>
-            <td>ทะเบียนรถ</td>
-            <td>วันที่</td>
+            <td>อาการ</td>
             <td>ลบ</td>
             <td>แก้ไข</td>
         </tr>
         @php
-            for ($x = 0; $x < 5; $x++) {
+            $z = ['เบรคสะบัด', 'คลัทช์เสีย', 'เกียร์หลุด', 'เครื่องสะดุด', 'เพลาหัก', 'สลักหาย'];
+            foreach ($z as $s) {
                 echo "<tr>
-        <td>บาส</td>
-        <td>ไข่เล็ก</td>
-        <td>000-0000000</td>
-        <td>112aa</td>
-        <td>32/13/9999</td>
-        <td><button type='button' class='btn btn-danger'>ลบ</button></td>
-        <td><button type='button' class='btn btn-secondary'>แก้ไข</button></td>
-    </tr>";
+    <td>$s</td>
+    <td><button type='button' class='btn btn-danger'>ลบ</button></td>
+    <td><button type='button' class='btn btn-secondary'>แก้ไข</button></td>
+</tr>";
             }
         @endphp
     </table>
-    <div class="d-flex justify-content-end mt2">
-        <a type="button" class="btn btn-success me2" href="{{ route('addcarcheck') }}">เพิ่ม</a>
-    </div>
 
 @endsection
--- resources/views/carrecive.blade.php
+++ resources/views/carrecive.blade.php
@@ -1,26 +1,35 @@
 @extends('layout')
-@section('title', 'Carrecive')
+@section('title', 'Carcheck')
 
 @section('content')
-    <h2>ประวัติอาการรถ</h2>
-
+    <h2>ข้อมูลรับรถ</h2>
     <p class="">ID รับรถ</p>
     <table class="table table-bordered text-center">
         <tr>
-            <td>อาการ</td>
+            <td>ชิ่อ</td>
+            <td>สกุล</td>
+            <td>เบอร์</td>
+            <td>ทะเบียนรถ</td>
+            <td>วันที่</td>
             <td>ลบ</td>
             <td>แก้ไข</td>
         </tr>
         @php
-            $z = ['เบรคสะบัด', 'คลัทช์เสีย', 'เกียร์หลุด', 'เครื่องสะดุด', 'เพลาหัก', 'สลักหาย'];
-            foreach ($z as $s) {
+            for ($x = 0; $x < 5; $x++) {
                 echo "<tr>
-    <td>$s</td>
-    <td><button type='button' class='btn btn-danger'>ลบ</button></td>
-    <td><button type='button' class='btn btn-secondary'>แก้ไข</button></td>
-</tr>";
+        <td>บาส</td>
+        <td>ไข่เล็ก</td>
+        <td>000-0000000</td>
+        <td>112aa</td>
+        <td>32/13/9999</td>
+        <td><button type='button' class='btn btn-danger'>ลบ</button></td>
+        <td><button type='button' class='btn btn-secondary'>แก้ไข</button></td>
+    </tr>";
             }
         @endphp
     </table>
+    <div class="d-flex justify-content-end mt2">
+        <a type="button" class="btn btn-success me2" href="{{ route('addcarcheck') }}">เพิ่ม</a>
+    </div>
 
 @endsection
