a07f85da843b6726ca4c64f508644e2e55f686e7
BigShowEiEi <bigshowgawin@gmail.com>
2024-03-15 22:15:22 +0700

    แก้ไขหน้า addcarcheck นิดหน่อย

--- resources/views/addcarcheck.blade.php
+++ resources/views/addcarcheck.blade.php
@@ -10,7 +10,8 @@
         <div class="">
             <center>
                 <h3>เลือกรถ</h3>
-                <select name="car_id" class="form-control">
+                <select name="car_id" class="form-select w-25" style="text-align:center">
+                    <option selected hidden >เลือกรถ</option>
                     @foreach($cars as $car)
                         <option value="{{ $car->carR_id }}">{{ $car->car_id }}</option>
                     @endforeach
@@ -21,7 +22,7 @@
         <div class="">
             <center>
                 <h3>อาการ</h3>
-                <input type="text" class="from-control" placeholder="อาการ" name="detail"> 
+                <input type="text" class="form-select w-25" style="text-align:center" placeholder="ระบุอาการ" name="detail"> 
             </center>
         </div><br>
         
