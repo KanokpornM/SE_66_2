e1317d2561917538331983e39ca81d043ad64098
MiaoWa23 <mamiao5477@gmail.com>
2024-03-19 18:38:50 +0700

    โชว์อาการตอนแก้ไข

--- resources/views/editrecive.blade.php
+++ resources/views/editrecive.blade.php
@@ -9,16 +9,22 @@
         @method('PUT')
 
         <center>
-        <div class="form-group">
-            <label for="checkcarstatus_id">สถานะ:</label>
-            <select class="form-select w-25" id="checkcarstatus_id" name="checkcarstatus_id">
-                <option value="1" {{ $carcheck->checkcarstatus_id == 1 ? 'selected' : '' }}>พัง</option>
-                <option value="2" {{ $carcheck->checkcarstatus_id == 2 ? 'selected' : '' }}>ไม่พัง</option>
-            </select>
-        </div>
-        <br>
-        <a href="{{ route('recive') }}" class="btn btn-danger">ยกเลิก</a>
-        <button type="submit" class="btn btn-success">บันทึก</button>
+            <div class="form-group">
+                <label for="detail">รายละเอียด:</label>
+                <input type="text" class="form-control" id="detail" name="detail" value="{{ $carcheck->detail }}"
+                    disabled>
+            </div>
+
+            <div class="form-group">
+                <label for="checkcarstatus_id">สถานะ:</label>
+                <select class="form-select w-25" id="checkcarstatus_id" name="checkcarstatus_id">
+                    <option value="1" {{ $carcheck->checkcarstatus_id == 1 ? 'selected' : '' }}>พัง</option>
+                    <option value="2" {{ $carcheck->checkcarstatus_id == 2 ? 'selected' : '' }}>ไม่พัง</option>
+                </select>
+            </div>
+            <br>
+            <a href="{{ route('recive') }}" class="btn btn-danger">ยกเลิก</a>
+            <button type="submit" class="btn btn-success">บันทึก</button>
         </center>
     </form>
 
